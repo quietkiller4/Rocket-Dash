1e483038f03973ac879aeee45229799832332489
quietkiller4 <kevinkellerjr@gmail.com>
2025-01-21 16:12:34 -0500

    MainMenu

--- Rocket Dash/Assets/Scripts/MainMenuScript.cs
+++ Rocket Dash/Assets/Scripts/MainMenuScript.cs
@@ -3,8 +3,24 @@ using UnityEngine.SceneManagement;
 
 public class MainMenuScript : MonoBehaviour
 {
-  public void PlayGame()
+    public void PlayGame()
     {
         SceneManager.LoadScene(1);
     }
+
+    public void OptionsMenu()
+    {
+        SceneManager.LoadScene("SettingMenu");
+    }
+
+    public void GoToMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
